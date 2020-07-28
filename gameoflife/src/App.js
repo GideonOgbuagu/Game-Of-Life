a55fd1b59c1b9ad1d9434962c5868cfbf7fbbeaa
Gideon <gideon@Mac-1s-MacBook-Pro.local>
2020-07-28 15:01:40 -0400

    initial display of grids

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
 
+const numRows = 30
+const numCols = 50
+
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0)) //reduce all the value of iteration to zeros with Array.from([1,2,3], x => x + x) // [2, 4, 6]
+
+    }
+
+    return rows;
+  })
+
+  console.log(grid)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Game of Life</h1>
+        <div>
+          {grid.map(rows => (
+            <div>
+              {rows}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
