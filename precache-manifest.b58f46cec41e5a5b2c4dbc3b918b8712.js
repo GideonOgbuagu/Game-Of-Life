5e623f682578c0251e2d660058f97609cf54c71e
Gideon <gideon@Mac-1s-MacBook-Pro.local>
2020-07-31 13:02:07 -0400

    Updates

---
+++ precache-manifest.b58f46cec41e5a5b2c4dbc3b918b8712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5ada379e4c1668f3a9d50a7f02939d",
+    "url": "/Game-Of-Life/index.html"
+  },
+  {
+    "revision": "c8178a3c95748890bb3c",
+    "url": "/Game-Of-Life/static/css/main.8b2b0ac5.chunk.css"
+  },
+  {
+    "revision": "3ea903e79edc475db662",
+    "url": "/Game-Of-Life/static/js/2.b6b4557e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Game-Of-Life/static/js/2.b6b4557e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8178a3c95748890bb3c",
+    "url": "/Game-Of-Life/static/js/main.899f3bc9.chunk.js"
+  },
+  {
+    "revision": "9850cd3620d024eeaf1d",
+    "url": "/Game-Of-Life/static/js/runtime-main.6f451d3f.js"
+  }
+]);
